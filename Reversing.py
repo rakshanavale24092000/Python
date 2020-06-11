26e5ddfb9cbaa033d318c4238a1d4fc105061d19
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-12 00:03:42 +0530

    Reversing

---
+++ Reversing.py
@@ -0,0 +1,9 @@
+#In python slicing  is used to grab a section of sting
+myString = "Hello World"
+slice=myString[1:6:1]
+#[Start_point : end_point : step_size]
+print(slice)
+
+#Reversing of myString
+reverse = myString[::-1]
+print(reverse)
