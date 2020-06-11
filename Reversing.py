04590a9122ee86a184db7fb289ad847b5b64fe36
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-12 00:05:01 +0530

    Update Reversing.py

--- Reversing.py
+++ Reversing.py
@@ -3,7 +3,9 @@ myString = "Hello World"
 slice=myString[1:6:1]
 #[Start_point : end_point : step_size]
 print(slice)
+#out put: ello
 
 #Reversing of myString
 reverse = myString[::-1]
 print(reverse)
+#out put: dlroW olleH
