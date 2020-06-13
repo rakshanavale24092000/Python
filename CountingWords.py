301164bf499ee3518de2ad5ce480582183681efc
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-14 00:11:16 +0530

    Update CountingWords.py

--- CountingWords.py
+++ CountingWords.py
@@ -1,5 +1,9 @@
+#split() method splits string into list. You can specify the seperator, if not specified the default seperator is taken as white space.
+#x="Hi this is a string" then  x.split("i") splits x as ['H',' th','s ','s a str','ng']
+
+
 myString = "Hello, this is a string"
-words = myString.split(" ")
+words = myString.split()
 
 length = len(words)
 print(f"Number of words are:{length}")
