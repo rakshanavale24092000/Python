5f71596f7672c41350fa7a6c91161b36a720296c
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-16 22:22:29 +0530

    ascending order

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+d = {'k1':100, 'k2':50 , 'k3':200, 'k4':80, 'k5':600}
+values = d.values()
+print(f'the values are {values}')
+list_values = list(values)
+print(f'the sorted values are {sorted(list_values)}')
+
+
+
+
+
