54fa00aba1883f4b3c7435153b1a9c51abe73fc4
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-17 22:50:41 +0530

    tuple

--- tuples.py
+++ tuples.py
@@ -1,8 +1,20 @@
+#Tuples are similar to lists but are immutable 
+
 tuple = (4,10,20,15,80)
+ paranthesi
+#unpacking of tuples in variables
 a,b,c,d,e = tuple
 sum = a+b+c+d+e
 print(sum)
+#output->129
+
+#tuples are immutable so to add elements at the end concatination is used.
+#tuples are constructed by comma operators with or without being enclosed by parantheses
 tuple = tuple + (sum,)
 print(tuple)
+#output-> (4, 10, 20, 15, 80, 129)
+
+#tuple containing only sum
 tuple = tuple[-1:]
 print(tuple)
+#output->(129,)
