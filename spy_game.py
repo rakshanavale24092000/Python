c78b4cd96ba05e4c41b3ba2d86d40274c321047a
rakshanavale24092000 <46890826+rakshanavale24092000@users.noreply.github.com>
2020-06-18 23:12:50 +0530

    spy_game

--- spy_game.py
+++ spy_game.py
@@ -1,3 +1,9 @@
+# If the list contains 0,0,7 in order then the function returns true
+# examples:  [1,2,5,0,0,7] ->True
+#            [1, 4, 2, 0, 7, 5, 0, 6, 7] -> True
+#            [2, 7, 0, 0, 3] -> False
+
+
 def spy_game(num):
     code = [0, 0, 7, 'x']
     for i in num:
@@ -7,5 +13,25 @@ def spy_game(num):
     return len(code) == 1
 
 
-nums = [1, 4, 2, 0, 0, 7]
+nums = [1, 0, 0, 6, 7]
 print(spy_game(nums))
+#output-> True
+
+# iteration 1:
+#     i = 1 , False
+
+# iteration 2:
+#     i = 0 , True
+#     0 from code is poped therefore code=[0, 7, 'x']
+
+# iteration 3:
+#     i = 0 , True
+#     0 from code is poped therefore code=[7, 'x']
+
+# iteration 4:
+#     i = 6 , False
+
+# iteration 3:
+#     i = 7 , True
+#     0 from code is poped therefore code=['x']
+#for loop breaks and length is 1 and hence True is returned
